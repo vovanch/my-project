7a963a4a0526dd6b8ee0f66a5ae5934d0c8cfa70
Volodymyr Voitovych <vvoitovych1@gmail.com>
2019-04-10 11:33:48 +0000

    Message

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
 <p>
-    Welcome <?php echo $_POST["Ім'я"]; ?><br>
-    Your password is registere: <?php echo $_POST["Пароль"]; ?>
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your password is registered: <?php echo $_POST["password"]; ?>
 </p>
--- js/index.js
+++ js/index.js
@@ -10,3 +10,6 @@ function submitForm(e) {
     .then( html => document.querySelector('.server-response')
                                 .innerHTML = html );
 }
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2))+"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
